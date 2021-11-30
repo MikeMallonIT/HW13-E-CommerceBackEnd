6f2600a5caab6799d9bfcedacba16d60066fd759
Mike Mallon <mallon127@gmail.com>
2021-11-30 10:33:38 -0600

    Update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
+// The `/api/categories` endpoints
 
 router.get('/', (req, res) => {
   try {
